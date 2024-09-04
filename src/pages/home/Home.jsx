a79d8c9086ff0d9016d0d0b83bff098f57307f2e
unknown <karthikeya3762@gmail.com>
2024-09-04 16:25:08 +0530

    add a fullstack developer title in home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
-import Profile from '../../assets/home.png'
-import { Link } from 'react-router-dom';
+import React from "react";
+import Profile from "../../assets/home.png";
+import { Link } from "react-router-dom";
 import { FaArrowRight } from "react-icons/fa";
-import './home.css'
-
+import "./home.css";
 
 const Home = () => {
   return (
@@ -13,19 +12,25 @@ const Home = () => {
       <div className="home_content">
         <div className="home_data">
           <h1 className="home_title">
-            <span>I'm Karthikeyan.</span> <br /> Mern Stack Developer
+            <span>I'm Karthikeyan.</span> <br /> Fullstack Developer
           </h1>
           <p className="home_description">
-            Transitioning from Non-IT background.I aim to develop proficient coding skills to enhance problem-solving capabilities,contribute to technology-driven solutions,and cultivate a successful career in software development.
+            Transitioning from Non-IT background.I aim to develop proficient
+            coding skills to enhance problem-solving capabilities,contribute to
+            technology-driven solutions,and cultivate a successful career in
+            software development.
           </p>
-          <Link to={'/about'} className='button'>
-            More About Me <span className='button_icon'><FaArrowRight /></span>
+          <Link to={"/about"} className="button">
+            More About Me{" "}
+            <span className="button_icon">
+              <FaArrowRight />
+            </span>
           </Link>
         </div>
       </div>
       <div className="color_block"></div>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
